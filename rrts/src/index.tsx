f7f7b484c329970f02ce85844b993dde17061116
Mateusz Karpiński <111981363+MateuszKar2@users.noreply.github.com>
2025-01-07 21:50:57 +0100

    add main file

--- rrts/src/index.tsx
+++ rrts/src/index.tsx
@@ -1,10 +1,30 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import ReactDOM from 'react-dom/client';
 
-class App extends React.Component {
-  render() {
-    return <div>Hi there</div>;
-  }
+interface AppProps {
+    color?: string;
 }
 
-ReactDOM.render(<App />, document.querySelector('#root'));
+class App extends React.Component<AppProps> {
+    state = { counter: 0 };
+
+    onIncrement = (): void => {
+        this.setState({ counter: this.state.counter + 1 });
+    };
+
+    onDecrement = (): void => {
+        this.setState({ counter: this.state.counter - 1});
+    };
+
+    render() {
+        return(
+            <div>
+                <button onClick={this.onIncrement}>Increment</button>
+                <button onClick={this.onDecrement}>Decrement</button>
+                {this.state.counter}
+            </div>
+        )
+    }
+}
+
+ReactDOM.createRoot(<App color="red" />, document.querySelector('#root'));
