858718aa894a61f1764c26bc1e38e2b0deac77b4
Mateusz Karpiński <111981363+MateuszKar2@users.noreply.github.com>
2025-01-10 12:25:45 +0100

    add file

--- src/index.tsx
+++ src/index.tsx
@@ -1,16 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { createStore, applyMiddleware } from 'redux';
+import { configureStore } from '@reduxjs/toolkit';
 import { Provider } from 'react-redux';
-import thunk from 'redux-thunk';
 import { App } from './components/App';
 import { reducers } from './reducers';
 
-const store = createStore(reducers, applyMiddleware(thunk));
+const store = configureStore({
+  reducer: reducers,
+})
+
 
 const root = ReactDOM.createRoot(document.querySelector('#root'));
 root.render(
   <Provider store={store}>
-        <App/>
-    </Provider>,
+    <App />
+  </Provider>
 );
